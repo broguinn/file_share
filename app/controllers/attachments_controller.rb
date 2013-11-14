5f7117df872f65650ce4c1c5978f50e30781ad1c
Braden OGuinn <bgoguinn@gmail.com>
2013-11-14 12:03:57 -0800

    remove test lines

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,9 +3,6 @@ class AttachmentsController < ApplicationController
     @attachment = Attachment.find(params[:id])
     if @attachment.package.authenticate(params[:token])
       @attachment.send_sender_email
-      #'https://s3-us-west-2.amazonaws.com/file-share-argon/attachments/files/000/000/015/original/04_Let_it_Go_Nah.mp3'
-      #'http://s3.amazonaws.com/file-share-argon/attachments/files/000/000/015/original/04_Let_it_Go_Nah.mp3?1384455498'
-      puts @attachment.file.url
       data = open @attachment.file.url
       send_data data.read, filename: @attachment.file_file_name, type: @attachment.file_content_type
     else
